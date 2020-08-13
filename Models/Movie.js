3c1fd41a7d51f3f2dd43b02b45567fce051ddab4
AhmadHirthani <hertani86@gmail.com>
2020-08-13 06:23:09 +0300

    fixMovieImages

--- Models/Movie.js
+++ Models/Movie.js
@@ -1,22 +1,14 @@
-function Movie(data){
-  this.title=data.title;
-  this.overview=data.overview;
-  this.average_votes=data.vote_average;
-  this.total_votes=data.vote_count;
-  this.image_url=data.poster_path;
-  this.popularity=data.popularity;
-  this.released_on=data.release_date;
- 
+function Movie(data) {
+    this.title = data.title;
+    this.overview = data.overview;
+    this.average_votes = data.vote_average;
+    this.total_votes = data.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+    this.popularity = data.popularity;
+    this.released_on = data.release_date;
+
 };
 
 
 
 exports.default = Movie;
-
-
-
-
-
-
-
-
